0ffbedf320697f182e06122f1c1bc4d6f7c68df9
Aziz <med.aziz.gharbii@gmail.com>
2023-05-06 03:25:42 -0400

    fix: update unit test

--- lua/tests/nodePackageCheck_spec.lua
+++ lua/tests/nodePackageCheck_spec.lua
@@ -1,15 +1,13 @@
 -- package.path = package.path .. ';/plugins/nodePackageCheck.nvim/?.lua'
 
-local npc = require("nodePackageCheck")
-
 describe("nodePackageCheck", function()
 	it("trim_string", function()
-		local trim_s = npc.Utils.trim_string("hello     world ")
+		local trim_s = require("nodePackageCheck.service").trim_string("hello     world ")
 		assert.True(trim_s == "helloworld")
 	end)
 
 	it("get_package_latest_version", function()
-		local version = npc.Utils.get_package_latest_version("typescript")
+		local version = require("nodePackageCheck.service").get_package_latest_version("typescript")
 		assert.are.equal(version, "4.9.5")
 	end)
 end)
