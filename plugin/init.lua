0e646a21bd04c1a2290aa5e6b430cfaee82cc26a
Aziz <med.aziz.gharbii@gmail.com>
2023-06-04 19:15:21 -0400

    feat: add vim cmd

--- plugin/init.lua
+++ plugin/init.lua
@@ -11,14 +11,14 @@ require("nodePackageCheck").Highlights.info_highlight()
 require("nodePackageCheck").Highlights.warning_highlight()
 --End
 
+-- Commands definition
 -- Clear buffer
-function Clear_highlights()
+Clear_highlights = function()
 	vim.api.nvim_buf_clear_namespace(0, require("nodePackageCheck").Config.get_namespace_id(), 0, -1)
 end
 
-vim.api.nvim_command(string.format("autocmd TextChanged,TextChangedI,BufWritePost <buffer> lua Clear_highlights()"))
+vim.cmd([[autocmd! TextChanged,TextChangedI, BufWritePost  * lua Clear_highlights()]])
 
--- Commands definition
 vim.api.nvim_create_user_command("NPCUpdateLineVersion", function()
 	Clear_highlights()
 	local _, error = pcall(require("nodePackageCheck").Utils.confirmation_to_update_line_version)
